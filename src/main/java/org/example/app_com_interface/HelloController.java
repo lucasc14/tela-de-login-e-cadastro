1db2d82175feeee9b823efe249f4032e16b18a9d
lucasc14 <lucas.c14.51@gmail.com>
2026-04-07 11:46:48 -0300

    Criando a controller

--- src/main/java/org/example/app_com_interface/HelloController.java
+++ src/main/java/org/example/app_com_interface/HelloController.java
@@ -1,14 +1,23 @@
 package org.example.app_com_interface;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    TextField txtUsuario ;
+    @FXML
+    PasswordField txtSenha;
+    @FXML
+    Button btnLogin;
+    @FXML
+    Label labelLogin;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Bem-vindo a Aplicação JavaFX!");
+    protected void onBtnLoginClick() {
+        labelLogin.setText("Login realizado com sucesso!");
     }
 }
